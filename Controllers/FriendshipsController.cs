297fdae6d41c49807c888cc3238b5918d497c050
Jérémi C <jeremichretien@gmail.com>
2023-05-01 16:45:01 -0400

    json file for friendship
    ajout des pages, controller pour la pfi et le model pour friendship.

---
+++ Controllers/FriendshipsController.cs
@@ -0,0 +1,27 @@
+﻿using ChatManager.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ChatManager.Controllers
+{
+    public class FriendshipsController : Controller
+    {
+        // GET: Friendships
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult GetUsersList(bool forceRefresh = false)
+        {
+            if (forceRefresh || OnlineUsers.HasChanged())
+            {
+                return PartialView(DB.Users.SortedUsers());
+            }
+            return null;
+        }
+    }
+}
