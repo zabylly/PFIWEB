944d7e35c93e5782bec415e943d06fa1125079fc
zabylly <117114627+zabylly@users.noreply.github.com>
2023-05-02 20:16:17 -0400

    Merge branch 'master' of https://github.com/zabylly/PFIWEB

---
+++ Models/Friendship.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ChatManager.Models
+{
+    public class Friendship
+    {
+        public int userId { get; set; }
+        public int friendId { get; set; }
+        public bool askFriend { get; set; }
+        public bool askedFriend { get; set; }
+        public bool deniedFriend { get; set; }
+    }
+}
