9c22dbe0621a355ceef9b87791dd193cec1cd89e
Jérémi C <jeremichretien@gmail.com>
2023-05-12 14:41:17 -0400

    fix id -> idUser

--- Models/FriendshipRepository.cs
+++ Models/FriendshipRepository.cs
@@ -40,7 +40,7 @@ namespace ChatManager.Models
         }
         public List<Friendship> GetListFriendshipWithNullRelation(int userId)
         {
-            IEnumerable<Friendship> friendships = ToList().Where(u => u.Id == userId);
+            IEnumerable<Friendship> friendships = ToList().Where(u => u.IdUser == userId);
             List<Friendship> friendshipsComplete = new List<Friendship>(friendships);
             bool IsInRelation(User user)
             {
@@ -83,7 +83,7 @@ namespace ChatManager.Models
                 demandeAmis.Id = base.Add(demandeAmis);
                 Friendship receveurDemande = new Friendship();
                 receveurDemande.IdUser = demandeAmis.IdFriend;
-                receveurDemande.IdFriend = demandeAmis.Id;
+                receveurDemande.IdFriend = demandeAmis.IdUser;
                 receveurDemande.FriendStatus = Friendship.RequestReceved;
                 receveurDemande.Id = base.Add(receveurDemande);
                 return demandeAmis;
@@ -134,7 +134,7 @@ namespace ChatManager.Models
         //}
         public Friendship FindFriendRelation(Friendship friendship)
         {
-            IEnumerable<Friendship> FriendTmp = ToList().Where(u => u.IdUser == friendship.IdFriend && u.IdFriend == friendship.Id);
+            IEnumerable<Friendship> FriendTmp = ToList().Where(u => u.IdUser == friendship.IdFriend && u.IdFriend == friendship.IdUser);
             if (FriendTmp.Count() > 0) return FriendTmp.First();
             else return null;
         }
