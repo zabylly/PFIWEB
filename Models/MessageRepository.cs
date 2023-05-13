f125d3803a1b97f5164cba4faaaed5002d745a47
zabylly <117114627+zabylly@users.noreply.github.com>
2023-05-13 11:31:56 -0400

    fix Get message chat

--- Models/MessageRepository.cs
+++ Models/MessageRepository.cs
@@ -7,9 +7,9 @@ namespace ChatManager.Models
 {
     public class MessageRepository : Repository<Message>
     {
-        public IEnumerable<Message> GetMessageUser(int id)
+        public IEnumerable<Message> GetMessageChat(int id,int idFriend)
         {
-            return ToList().Where(u => u.IdSender == id || u.IdRecever == id ).OrderBy(u=>u.DateSent);      
+            return ToList().Where(u => u.IdSender == id && u.IdRecever == idFriend || u.IdSender == idFriend && u.IdRecever == id).OrderBy(u=>u.DateSent);      
         }
 
         public string SaveMessage(int idSender, int idRecever, string text)
